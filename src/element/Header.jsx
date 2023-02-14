652599e347835b402189322b6bad284714ebd8d0
Sami Skouri <sami.skouri@gmail.com>
2023-02-14 17:03:01 +0100

    fix Css and add support underline for the nav menu

--- src/element/Header.jsx
+++ src/element/Header.jsx
@@ -1,17 +1,35 @@
 import logo from "../img/logo.svg"
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
 //import AsyncImage from "./asyncImageLoading";
 
 
 function SiteHeader(){
+
+    const Location = useLocation();
+    const underlineNav = (targetLocation , world)=>{
+        
+
+        const firstLocation = Location.pathname.split("/");
+  
+        if(firstLocation[1].toLowerCase()=== targetLocation.toLowerCase()){
+         
+            return (<u>{world}</u>)
+        }else{
+            return (<>{world}</>)
+
+        }
+        
+    }
+
     return ( 
         <header className="headerClass">
      <Link to="/" >
      <img src={logo} alt="logo" />
      </Link>
         <nav className="Head_Nav">
-            <Link to="/">Acceuil</Link>
-            <Link to="/About/">A Propos</Link>
+            <Link  to="/">{underlineNav( "","Acceuil")}</Link>
+            <Link  to="/About/">{underlineNav("About", "A Propos")}</Link>
         </nav>
         </header>
    );
